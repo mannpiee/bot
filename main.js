eb101668202538d39f6e52f9e0bd62214a20712a
Manxtodd <74522163+mxdies@users.noreply.github.com>
2022-03-18 22:51:31 +0700

    update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const Command = require('./lib/command.js');
 const WAConnection = require('./lib/waconnection.js');
 
 global.baileys = require('@adiwajshing/baileys');
-global.botinfo = require('./src/json/botInfo.json');
-global.functions = new Functions();
+global.bot = require('./src/json/botInfo.json');
+global.f = new Functions();
 global.client = new WAConnection();
 global.cmd = new Command(client, botinfo, functions);
 global.logo = []
